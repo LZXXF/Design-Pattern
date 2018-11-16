8f5917b9aa34cb7616247d1a25ae095651faf9ff
lzxxf <lican823462517@hotmail.com>
2018-11-15 18:04:35 -0600

    Singleton pattern implementation with enumeration

---
+++ src/com/cl/singleton/SingletonDemo4.java
@@ -0,0 +1,16 @@
+package com.cl.singleton;
+
+/**
+ * Singleton pattern implementation with enumeration
+ * @author Can Li
+ */
+
+public enum SingletonDemo4 {
+
+    INSTANCE;
+
+    public void singletonOperation() {
+
+    }
+
+}
