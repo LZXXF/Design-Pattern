a22c88b7d00d85194896b8c01b6baa20aee639ce
lzxxf <lican823462517@hotmail.com>
2018-11-15 18:11:36 -0600

    Test for Singleton pattern

---
+++ src/com/cl/singleton/Test.java
@@ -0,0 +1,15 @@
+package com.cl.singleton;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        SingletonDemo1 s1 = SingletonDemo1.getInstance();
+        SingletonDemo1 s2 = SingletonDemo1.getInstance();
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        System.out.println(SingletonDemo4.INSTANCE == SingletonDemo4.INSTANCE);
+    }
+}
